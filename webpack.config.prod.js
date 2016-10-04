224dd59df2c64930bbf0db0ccc12f979478cc5e0
Alexey <grimzest@gmail.com>
2016-10-04 15:11:24 +0300

    init

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: './src/index',
+  
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.UglifyJsPlugin({
+        compressor: { warnings: false }
+    }),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+    }),
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('css!stylus')
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css')
+      },
+    ]
+  },
+
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+};
